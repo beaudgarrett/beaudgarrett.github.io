537a2f253d262e7de5e438c41a5bae61bb6bd80b
beaugarrett <tobeaugarrett@gmail.com>
2026-04-09 09:19:37 -0400

    fix: tighten constellation letter spacing

--- script.js
+++ script.js
@@ -108,8 +108,8 @@
     for (var ni = 0; ni < shuffled.length; ni++) {
       var pt = shuffled[ni];
       // Map text pixel bounding box directly onto the h1 element rect
-      var tx = rect.left - 60 + ((pt.x - minPx) / pxW) * rect.width;
-      var ty = rect.top + ((pt.y - minPy) / pxH) * rect.height;
+      var tx = rect.left - 60 + ((pt.x - minPx) / pxW) * rect.width * 0.8;
+      var ty = rect.top + ((pt.y - minPy) / pxH) * rect.height * 0.85;
 
       nameStars.push({
         // Start scattered randomly across viewport
